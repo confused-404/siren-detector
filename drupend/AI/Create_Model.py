87bddf2fb4d688faad50f0677d0660d6517c3fd9
JA7777777 <jacob.a715@slcstudents.org>
2026-03-03 21:29:30 -0700

    Added model creation script

---
+++ drupend/AI/Create_Model.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+#note:6000 samples first
+input_dimension = 16000
+num_classes = 3
+dropout = 0.45 #0.3 OG
+def create_mlp_model(input_dimension, num_classes, dropout):
+
+    model = keras.Sequential([
+        keras.Input(shape=(input_dimension,)),
+
+        layers.Dense(2000, activation='relu', name='hidden_layer_1'),
+        layers.Dropout(dropout),
+
+        layers.Dense(500, activation='relu', name='hidden_layer_2'),
+        layers.Dropout(dropout-0.1),
+
+        layers.Dense(125, activation='relu', name='hidden_layer_3'),
+        layers.Dropout(dropout-0.2),
+
+        layers.Dense(25, activation='relu', name='hidden_layer_4'),
+        layers.Dropout(dropout-0.3),
+
+        layers.Dense(num_classes, activation='softmax', name='output_layer')
+    ])
+
+
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+
+mlp_model = create_mlp_model(input_dimension, num_classes, dropout)
+
+mlp_model.save("Car_Alert_Model.h5")
+# Display the model summary
+mlp_model.summary()
+print("Made and Saved")
